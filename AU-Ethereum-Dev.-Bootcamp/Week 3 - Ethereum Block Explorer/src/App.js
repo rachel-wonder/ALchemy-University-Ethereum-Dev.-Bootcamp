6d30611c6c26c54044a5aa3b0a337c1b2d00e8bf
00Q00 <100677872+00Q00@users.noreply.github.com>
2024-07-21 16:32:07 +0800

    add Week3 file

---
+++ AU-Ethereum-Dev.-Bootcamp/Week 3 - Ethereum Block Explorer/src/App.js
@@ -0,0 +1,57 @@
+import { Alchemy, Network } from 'alchemy-sdk';
+import { useEffect, useState } from 'react';
+
+import './App.css';
+
+// Refer to the README doc for more information about using API
+// keys in client-side code. You should never do this in production
+// level code.
+const settings = {
+  apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
+  network: Network.ETH_MAINNET,
+};
+
+
+// In this week's lessons we used ethers.js. Here we are using the
+// Alchemy SDK is an umbrella library with several different packages.
+//
+// You can read more about the packages here:
+//   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
+const alchemy = new Alchemy(settings);
+
+function App() {
+  const [blockNumber, setBlockNumber] = useState();
+  const [timestamp, setTimestamp] = useState();
+  const [transactions, setTransactions] = useState();
+
+  useEffect(() => {
+    async function getBlockNumber() {
+      setBlockNumber(await alchemy.core.getBlockNumber());
+    }
+
+    getBlockNumber();
+  },[]);
+
+  useEffect(() => {
+    async function getTransactions() {
+      const block = await alchemy.core.getBlockWithTransactions(blockNumber);
+      setTimestamp(block.timestamp);
+      setTransactions(block.transactions); 
+    }
+
+      getTransactions();
+  },[blockNumber]);
+
+
+
+  return (
+    <div className="App">
+      <h1>Block Number: {blockNumber}</h1>
+      <h2>Timestamp: {new Date(timestamp * 1_000).toLocaleString()}</h2>
+      <h3>Transactions: {transactions && transactions.length}</h3>
+    </div>
+    );
+
+}
+
+export default App;
